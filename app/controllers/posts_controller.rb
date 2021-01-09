7ff11f3c0106c0d0f7817009ceed19d44c510f83
momo28029 <momotis915@gmail.com>
2021-01-09 23:31:25 +0900

    メモ投稿機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,11 @@ class PostsController < ApplicationController
  end
 
  def create
+  post = Post.create(content: params[:content], checked: false)
+  render json:{ post: post }
+ end
+
+ def create
   Post.create(content: params[:content])
   redirect_to action: :index
  end
